72763e3256cbb5c44a04392e5c174a01cca0f2df
Cody Brubaker and Chris Clifford <chris@>
2016-12-13 10:53:15 -0800

    add front end for manager portal

--- app.rb
+++ app.rb
@@ -7,6 +7,38 @@ require('./lib/product')
 require('pg')
 
 get('/') do
-  
   erb(:index)
+end
+
+get('/products') do
+  @products = Product.all()
+  erb(:products)
+end
+
+post('/products') do
+  name = params.fetch('name')
+  price = params.fetch('price').to_f
+  Product.create({:name => name, :price => price})
+  @products = Product.all()
+  erb(:products)
+end
+
+get('/product/:id') do
+  @product = Product.find(params.fetch('id').to_i)
+  erb(:product)
+end
+
+patch('/product/:id') do
+  @product = Product.find(params.fetch('id').to_i)
+  name = params.fetch('name')
+  price = params.fetch('price').to_f
+  @product.update({:name => name, :price => price})
+  erb(:product)
+end
+
+delete('/product/:id') do
+  @product = Product.find(params.fetch('id').to_i)
+  @product.destroy()
+  @products = Product.all
+  erb(:products)
 end
